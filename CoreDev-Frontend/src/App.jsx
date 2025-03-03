e48d636f0f1ad3cc276f25f16cb9d970ca35a1b5
Jaylord Manila <jaylordmanila.tech@gmail.com>
2025-03-03 12:20:52 +0800

    add team page

--- CoreDev-Frontend/src/App.jsx
+++ CoreDev-Frontend/src/App.jsx
@@ -1,17 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "./layout/MainLayout";
-import { lazy, Suspense } from "react";
+import { lazy } from "react";
 
 const HomePage = lazy(() => import('./pages/Home/Home'));
 const CareerPage = lazy(() => import('./pages/Careers/Careers'));
 const SoftwarePage = lazy(() => import('./pages/Products/Software'));
 const SoftwareProductPage = lazy(() => import('./pages/Products/SoftwareProduct/SoftwareProduct'));
+const IAccsPage = lazy(() => import('./pages/Products/SoftwareProduct/iAccs'));
 const HardwarePage = lazy(() => import('./pages/Products/Hardware'));
 const HardwareProductPage = lazy(() => import('./pages/Products/HardwareProd/Product'));
 const ProductDetailsPage = lazy(() => import('./pages/Products/HardwareProd/ProductDetails'));
 const ClientPage = lazy(() => import('./pages/Clients/Clients'));
 const ContactPage = lazy(() => import('./pages/contactus/Contact-us'));
 const AboutPage = lazy(() => import('./pages/WhoweAre/WhoWeAre'));
+const OurTeamPage = lazy(() => import('./pages/OurTeam/OurTeam'));
 const PartnerPage = lazy(() => import ('./pages/Partners/Partners'));
 
 
@@ -25,11 +27,13 @@ function App() {
           <Route path="/Products/Software" element={<SoftwarePage/>}></Route>
           <Route path="/Products/Hardware" element={<HardwarePage/>}></Route>
           <Route path="/Products/Software/:product" element={<SoftwareProductPage/>}></Route>
+          <Route path="/Products/Software/iAccs" element={<IAccsPage/>}></Route>
           <Route path="/Clients" element={<ClientPage/>}></Route>
           <Route path="/partners" element={<PartnerPage/>}></Route>
           <Route path="/careers" element={<CareerPage/>}></Route>
           <Route path="/Contact-us" element={<ContactPage/>}></Route>
           <Route path="/about/who-we-are" element={<AboutPage/>}></Route>
+          <Route path="/about/our-team" element={<OurTeamPage/>}></Route>
           <Route path="/Products/Hardware/:category" element={<HardwareProductPage/>}></Route>
           <Route path="/Products/Hardware/details" element={<ProductDetailsPage />} > </Route>
         </Route>
---
+++ CoreDev-Frontend/src/pages/OurTeam/OurTeam.jsx
@@ -0,0 +1,95 @@
+/* eslint-disable react/prop-types */
+import "./OurTeam.css";
+import { motion } from "framer-motion";
+import { FaEnvelope } from "react-icons/fa";
+
+function OurTeam() {
+    const Teams = [
+        {
+            name: "Al A. Caputol",
+            position: "Chief Executive Officer",
+            image: "/assets/Al A.png",
+        },
+        {
+            name: "Rosendo Requino Jr.",
+            position: "Chief Operating Officer",
+            image: "/assets/rosendo.png",
+        },
+        {
+            name: "Bernardo Pangilinan",
+            position: "Chief Software Architect",
+            image: "/assets/bernardo.png",
+        },
+        {
+            name: "Mark Anthony del Coro",
+            position: "R&D Head",
+            image: "/assets/mark.png",
+        },
+        {
+            name: "Charrie Caputol",
+            position: "HR Director",
+            image: "/assets/charrie.png",
+        },
+    ];
+    return (
+        <div className="careerCont">
+            <div className="imageWrap">
+                <div className="textcont">
+                    <h2>OUR TEAM</h2>
+                </div>
+            </div>
+            <motion.div className="inner">
+                <h2 style={{ textAlign: "center" }}>
+                    Pillars of <span>CORE DEV SOLUTIONS INC.</span>
+                </h2>
+                <motion.div
+                    layout
+                    whileTap={{ scale: 0.9 }}
+                    initial={{ opacity: 0, scale: 0.8 }}
+                    whileInView={{ opacity: 1, scale: 1 }}
+                    transition={{
+                        layout: { duration: 0.3 },
+                        type: "",
+                        stiffness: 200,
+                        damping: 10,
+                        duration: 0.3,
+                        delay: 0.2,
+                    }}
+                    className="team-wrapper"
+                >
+                    {Teams.map((team, index) => (
+                        <TeamCard
+                            key={index}
+                            name={team.name}
+                            position={team.position}
+                            image={team.image}
+                        />
+                    ))}
+                </motion.div>
+            </motion.div>
+        </div>
+    );
+}
+
+const TeamCard = ({ name, position, image }) => {
+    return (
+        <div className="team">
+            <div className="image">
+                <img src={image} alt="" />
+            </div>
+            <div className="team-info">
+                <div className="team-name info">
+                    <h3>{name}</h3>
+                </div>
+                <div className="team-position info">
+                    <p>{position}</p>
+                </div>
+                <div className="message-icon">
+                    <FaEnvelope />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default OurTeam;
