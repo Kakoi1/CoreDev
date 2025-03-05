8945cba42393775c0d3ae973c48e7a79d89f624c
Kakoi1 <lopezrolandshane@gmail.com>
2025-03-05 12:32:33 +0800

    client page redesign

--- CoreDev-Frontend/src/pages/Products/Software.jsx
+++ CoreDev-Frontend/src/pages/Products/Software.jsx
@@ -163,6 +163,7 @@ const Software = () => {
             >
                 <div className="IacWrap">
                     <motion.img
+                     onClick={() => window.open('https://www.coredev.ph/iaccs/', '_blank') }
                         whileHover={{ scale: 1.2 }}
                         whileTap={{ scale: 0.9 }}
                         transition={{ duration: 0.3 }}
@@ -173,7 +174,12 @@ const Software = () => {
                         height={74}
                     />
                     <div>
-                        <h4 className="iaccs-title">iAccs 2013</h4>
+                        <motion.h4
+                         onClick={() => window.open('https://www.coredev.ph/iaccs/', '_blank') }
+                          whileHover={{ scale: 1.03 }}
+                        whileTap={{ scale: 1.0 }}
+                        transition={{ duration: 0.3 }}
+                        className="iaccs-title">iAccs 2013</motion.h4>
                         <p>
                             The Most Trusted and Secured Integrated accounting
                             and
