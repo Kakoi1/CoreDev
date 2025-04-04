aacdde571b4fe79f2d7630d902dacac917c72c98
Jaylord Manila <jaylordmanila.tech@gmail.com>
2025-04-04 16:33:00 +0800

    integrate iaccs feature in software page

--- CoreDev-Frontend/src/pages/Products/Software.jsx
+++ CoreDev-Frontend/src/pages/Products/Software.jsx
@@ -1,151 +1,14 @@
 import "./Software.css";
-import { RiCodepenFill } from "react-icons/ri";
-import { IoMdOpen } from "react-icons/io";
-import { motion } from "framer-motion";
+import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io";
+import { AnimatePresence, motion } from "framer-motion";
+import IAccs from "./SoftwareProduct/iAccs";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { softwareProducts } from "./data";
 
 const Software = () => {
     const navigate = useNavigate();
-
-    const second = [
-        {
-            picUrl: "/assets/hris.png",
-            title: "Orange Pay Plus (HRIS)",
-            icon: <RiCodepenFill />,
-            description:
-                "Your all in one HR Solutions System is here! Go paperless with your HR files and expedite your payroll process. Subscribe to OrangePay+ (HRIS) now.",
-            feature: [
-                "Stand alone system",
-                "A complete paperless HR Management Solutions for SMBs and Enterprise Businesses.",
-                "Complete ease on managing employee info and payroll computation",
-                "Convenient employee access to their info and payslip",
-                "And many other very helpful features",
-            ],
-        },
-        {
-            picUrl: "/assets/eservices.png",
-            title: "E-Services Portal",
-            icon: <RiCodepenFill />,
-            description:
-                "Provide your members/clients the access to monitor their accounts and send loan applications online securely at their most convenient time and place. Use E-Services Portal now!",
-            feature: [
-                "Real time inquiry of Accounts (Loans and Deposits)",
-                "View and Print Statement of Accounts",
-                "Apply for a Loan Online",
-                "Integrated Portal with other E-Services",
-                "Mobile Responsive",
-            ],
-        },
-        {
-            picUrl: "/assets/voting.png",
-            title: "Online Voting System",
-            icon: <RiCodepenFill />,
-            description:
-                "Fast and secured way to cast your votes and readily generate results within a few clicks.",
-            feature: [
-                "Stand alone system",
-                "Automated Voting System",
-                "Faster voting Experience",
-                "Able to vote anywhere and anytime",
-                "Flexible, Convenient and Secured.",
-            ],
-        },
-        {
-            picUrl: "/assets/que.png",
-            title: "Online Queuing System",
-            icon: <RiCodepenFill />,
-            description:
-                "We care for you! To avoid a large crowd, let your client queue anytime anywhere with our Online Queuing System.",
-            feature: [
-                "Stand alone system",
-                "Members can queue online anytime and anywhere",
-                "Can be connected to a Kiosk Terminal",
-                "Queuing Monitor with running text announcements and video",
-                "Has a dashboard for the Admin",
-                "Mobile responsive",
-            ],
-        },
-        {
-            picUrl: "/assets/pos.png",
-            title: "Point-of-Sale",
-            icon: <RiCodepenFill />,
-            description:
-                "Meet the most versatile BIR accredited Point-of-Sale System. Make fast and hassle-free store transactions and easily award your customer with its Reward feature.",
-            feature: [
-                "Item Maintenance",
-                "Receiving/Return",
-                "Point of Sale",
-                "Inventory",
-                "Stocks Warehousing/Transfer",
-                "Purchase Order",
-                "Supplier Maintenance",
-                "Physical Inventory",
-                "Ordering System",
-            ],
-        },
-
-        {
-            picUrl: "/assets/online-membership-system.png",
-            title: "Online Membership System",
-            icon: <RiCodepenFill />,
-            description:
-                "An easy and convenient way to enroll new members of your cooperative. Allow future clients to fill out application forms, attend webinars and submit documents online via the Online Membership System",
-            feature: [
-                "Stand alone System ",
-                "Allow online membership application",
-                "Allow applicant to upload supporting documents",
-                "Alloe applicant to take selfie photo for identification",
-                "Capable of uploading video seminar",
-                "Applicant will be able to monitor the status of membership application",
-                "Capable for sending application updates via E-mail or SMS",
-                "Mobile Responsive",
-            ],
-        },
-        {
-            picUrl: "/assets/online-help-desk.png",
-            title: "Online Help Desk System",
-            icon: <RiCodepenFill />,
-            description:
-                " Manage your client's concerns by letting them raise and resolve their issues online via our Online Help Desk System",
-            feature: [
-                "Allow members to raise issues and concerns with ticket number online ",
-                "Capable for sending ticket updates via E-Mail or SMS",
-                "Members can reply and upload files on the specific concerns",
-                "Members can monitor ticket status",
-                "Mobile Responsive",
-            ],
-        },
-        {
-            picUrl: "/assets/coop-wallet.png",
-            title: "Coop Wallet",
-            icon: <RiCodepenFill />,
-            description:
-                "A web Application that allows you to check, transfer, save and withdraw your funds.",
-            feature: [
-                "Can add/deposit cash",
-                "Can WithdrawCash",
-                "Can Apply for an Instant Loan",
-                "Can be Customized",
-                "Mobile Responsive",
-            ],
-        },
-        {
-            picUrl: "/assets/online-payment-gateway.png",
-            title: "Online Payment Gateway",
-            icon: <RiCodepenFill />,
-            description:
-                "Allow your members/clients to enjoy their quality time by letting them pay online with our Online Payment Gateway and increase your collection efficiency.",
-            feature: [
-                "Easy access to current balances",
-                "Secure online payment solutions",
-                "Online Banking",
-                "Bank Over-the-Counter(OTC)",
-                "Over-the-counter Nonbanks/Bayad Centers",
-                "Auto posting to current accounts",
-                "Mobile Responsive",
-            ],
-        },
-    ];
+    const [showFeature, setShowFeature] = useState(false);
 
     return (
         <div className="SoftwareContainer">
@@ -171,24 +34,46 @@ const Software = () => {
                     />
                     <div>
                         <motion.h4
-                          whileHover={{ scale: 1.03 }}
-                        whileTap={{ scale: 1.0 }}
-                        transition={{ duration: 0.3 }}
-                        className="iaccs-title">iAccs 2013</motion.h4>
+                            whileHover={{ scale: 1.03 }}
+                            whileTap={{ scale: 1.0 }}
+                            transition={{ duration: 0.3 }}
+                            className="iaccs-title"
+                        >
+                            iAccs 2013
+                        </motion.h4>
                         <p>
                             The Most Trusted and Secured Integrated accounting
-                            and
-                            banking system designed for bank and cooperative{" "}
-                             needs for more than 3 decades!
+                            and banking system designed for bank and cooperative{" "}
+                            needs for more than 3 decades!
                         </p>
-                        <button className="IacWrap-btn" onClick={() => navigate("/Products/Software/iAccs")}>Explore More <IoMdOpen className="icon "/></button>
+                        <button
+                            className="IacWrap-btn"
+                            onClick={() => setShowFeature(!showFeature)}
+                        >
+                            {showFeature ? (
+                                <>
+                                    Explore Less
+                                    <IoIosArrowUp className="icon " />
+                                </>
+                            ) : (
+                                <>
+                                    Explore More
+                                    <IoIosArrowDown className="icon " />
+                                </>
+                            )}
+                        </button>
+                        <AnimatePresence>
+                            {showFeature && <IAccs />}
+                        </AnimatePresence>
                     </div>
                 </div>
             </motion.div>
 
-            <h1 className="product-title"><span>Our</span> Products</h1>
+            <h1 className="product-title">
+                <span>Our</span> Products
+            </h1>
             <div className="pastProj">
-                {second.map((secondItem, index) => (
+                {softwareProducts.map((secondItem, index) => (
                     <motion.div
                         layout
                         whileTap={{ scale: 0.9 }}
