f63ab3e4cd0e69cf42b60cb4554b20623f04295a
Jaylord Manila <jaylordmanila.tech@gmail.com>
2025-04-28 09:57:00 +0800

    home hero section redesign

--- CoreDev-Frontend/src/pages/Home/Home.jsx
+++ CoreDev-Frontend/src/pages/Home/Home.jsx
@@ -1,18 +1,12 @@
 import './Home.css';        
-import teamPhoto from '../../assets/coreDev-Team-Edited-2.png';
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import ParticlesComponent from './ParticlesComponent';
+import HeroComponent from './HeroComponent';
 import Carousel from './Carousel';
-import { FaLaptopCode } from "react-icons/fa";
-import { BiServer } from "react-icons/bi";
-import { BsHeadset } from "react-icons/bs";
 import { useState, useEffect } from 'react';
 import { easeInOut, motion } from 'framer-motion';
 import pcIcon from '../../assets/pc-icon.png';
 import { FiTool } from "react-icons/fi";
-import { TfiEmail } from "react-icons/tfi";
-import { FaFacebook } from "react-icons/fa";
 import Partners from '../Partners/Partners'
 
 const MAX_RETRIES = 3; // Maximum retry attempts
@@ -52,77 +46,13 @@ const Home = () => {
           fetchLogo(retryCount + 1);
         }, [retryCount]);
 
-    const [services] = useState([
-        {
-            name: "Software Development",
-            icon: <FaLaptopCode className='icon' />,
-            description: "We offer more than just accounting and banking software solutions – our diverse software products are tailored to meet your needs.",
-            label: "Explore our Software Solutions",
-            link: '/Products/Software'
-        },
-        {
-            name: "Hardware Distributor",
-            icon: <BiServer className='icon' />,
-            description: "We've broadened our product range to meet our clients' demands, offering computer peripherals, servers, and different types of printers like Passbook and POS printers.",
-            label: "View Hardware Listing",
-             link: '/Products/Hardware'
-        },
-        {   
-            name: "Service Provider",
-            icon: <BsHeadset className='icon' />,
-            description: "We provide 24/7 technical support to our valued clients. Feel free to contact us any time of the day. We are at your service.",
-            label: "Get in touch",
-             link: '/Contact-us'
-        }
-    ]);
-
+    
     
     return (
         <div className="home-container">
           
-            <ParticlesComponent />
+            < HeroComponent/>
 
-            <div className='services-wrapper'>
-                <h1 className='service-header'><span>OUR</span> SERVICES</h1>
-                <div className='service-list'>
-                    {services.map((item, index) => (
-                        <motion.div 
-                            key={index} 
-                            className='service-item'
-                            initial={{ opacity: 0 }}
-                            whileInView={{ opacity: 1 }}
-                            transition={{ ease: easeInOut, duration: 0.6, delay: index * 0.2 }}  
-                        >
-                            <div className='top'>
-                                {item.icon}
-                                <h3 className='service-item-header'>{item.name}</h3>
-                                <motion.p 
-                                    className='description'
-                                    initial={{ opacity: 0, y: 20 }}
-                                    whileInView={{ opacity: 1, y: 0 }}
-                                    transition={{ duration: 0.6, ease: easeInOut, delay: index * 0.3 }}
-                                >
-                                    {item.description}
-                                </motion.p>
-                            </div>
-                            <div className='bottom'>
-                                <motion.button
-                                  onClick={() =>
-                                    navigate(item.link)
-                                  }
-                                    type='button'
-                                    className='outlined-button' // Added class for styling
-                                    whileHover={{ scale: 1.05 }}
-                                    whileTap={{ scale: 0.95 }}
-                                    transition={{ duration: 0.3 }}
-                                >               
-                                    {item.label}
-                                </motion.button>
-                            </div>            
-                        </motion.div>
-                    ))}
-                </div>
-            </div>
             <div className='sido'>
                 <div className="join-us-wrapper">
                     <div className='left'>
@@ -140,7 +70,7 @@ const Home = () => {
                             transition={{ duration: 0.6, ease: easeInOut, delay: 0.4 }}
                         >
                             We believe that our people are our greatest asset. At coreDev, we foster a culture of collaboration, 
-                            innovation, and mutual respect. We're always looking for talented individuals who share our values and are 
+                            innovation, and mutual respect. We&apos;re always looking for talented individuals who share our values and are 
                             excited to contribute to our mission.
                         </motion.p>
                         <motion.button 
