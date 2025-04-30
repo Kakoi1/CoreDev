60f48644464f4d4efa902470eb0ffc959a69e939
Jaylord Manila <jaylordmanila.tech@gmail.com>
2025-04-30 22:30:58 +0800

    add reusable ui components

---
+++ CoreDev-Frontend/src/component/ui/index.jsx
@@ -0,0 +1,4 @@
+export { Button } from "./button/button";
+export { Tooltip } from "./tooltip/tooltip";
+export { Loading } from "./loading/loading";
+export { Badge } from "./badge/badge"
---
+++ CoreDev-Frontend/src/component/ui/loading/loading.jsx
@@ -0,0 +1,18 @@
+export const Loading = () => {
+    return (
+        <div className="loader-container">
+            <div className="loadingio-spinner-gear-nq4q5u6dq7r">
+                <div className="ldio-x2uulkbinbj">
+                    <div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
