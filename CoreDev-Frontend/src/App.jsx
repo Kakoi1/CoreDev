5eb7b2bdbe0eeb2fbb2221ebc225c41a25af8342
Jaylord Manila <jaylordmanila.tech@gmail.com>
2025-05-30 13:43:46 +0800

    add TanStack query for data fetching and caching

--- CoreDev-Frontend/src/App.jsx
+++ CoreDev-Frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import { BrowserRouter } from "react-router-dom";
 import NoticeBanner from "./components/Footer/NoticeBanner";
 import { AppRoutes } from "./routes/AppRoutes";
+import { queryClient } from "lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 function App() {
     return (
-        <BrowserRouter>
-            <NoticeBanner />
-            <AppRoutes />
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <NoticeBanner />
+                <AppRoutes />
+            </BrowserRouter>
+            <ReactQueryDevtools initialIsOpen={false}/>
+        </QueryClientProvider>
     );
 }
 
