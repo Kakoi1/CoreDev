826ce83d9879071a39dd4b8ac9e596923273b35b
Jaylord Manila <jaylordmanila.tech@gmail.com>
2025-06-11 08:54:32 +0800

    update Vite config to load environment variables for API proxy target

--- CoreDev-Frontend/vite.config.js
+++ CoreDev-Frontend/vite.config.js
@@ -1,16 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+import process from "node:process"
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-    plugins: [react(), tsconfigPaths()],
-    server: {
-        proxy: {
-            "/orangepay-api": {
-                target: "https://staging-coredev.orangepayplus.com",
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/orangepay-api/, "/"),
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd())
+    return {
+        plugins: [react(), tsconfigPaths()],
+        server: {
+            proxy: {
+                "/orangepay-api": {
+                    target: env.VITE_ORANGE_PAYPLUS_URL,
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/orangepay-api/, "/"),
+                },
             },
         },
-    },
+    }
 });
